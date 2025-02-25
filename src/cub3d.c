aeb71d532f20958fb9ca85e8476e125683aed29d
ZekaEu <zeka.surf@hotmail.com>
2025-02-25 20:14:53 -0300

    wip: implement ft_hook; tbd: draw walls and move player

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,11 +6,34 @@
 /*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 19:58:10 by dsayumi-          #+#    #+#             */
-/*   Updated: 2025/02/25 20:05:08 by joscarlo         ###   ########.fr       */
+/*   Updated: 2025/02/25 20:13:58 by joscarlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
+void	ft_hook(void *param)
+{
+	t_cub3d	*cub3d;
+
+	cub3d = param;
+	// draw_wallpaper(cub3d, cub3d->floor_color, cub3d->ceiling_color);
+	// distance_rays(cub3d);
+	if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_ESCAPE))
+		mlx_close_window(cub3d->mlx_ptr);
+	// if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_W))
+	// 	move_player(cub3d, PLAYER_SPEED, 0);
+	// if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_S))
+	// 	move_player(cub3d, -PLAYER_SPEED, 0);
+	// if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_A))
+	// 	move_player(cub3d, -PLAYER_SPEED, 1);
+	// if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_D))
+	// 	move_player(cub3d, -PLAYER_SPEED, -1);
+	// if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_RIGHT))
+	// 	rotate_player(cub3d, PLAYER_ROTATE_SPEED);
+	// if (mlx_is_key_down(cub3d->mlx_ptr, MLX_KEY_LEFT))
+	// 	rotate_player(cub3d, -PLAYER_ROTATE_SPEED);
+}
 
 int	main(int argc, char **argv)
 {
