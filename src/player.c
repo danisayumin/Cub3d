a8af9f8b5e997c8f62160bb4a72024a6e3ccdac2
ZekaEu <zeka.surf@hotmail.com>
2025-02-25 21:16:28 -0300

    feat: implement rotate_player method at player.c; tbd: distance_rays method

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 18:52:21 by joscarlo          #+#    #+#             */
-/*   Updated: 2025/02/25 20:36:05 by joscarlo         ###   ########.fr       */
+/*   Updated: 2025/02/25 21:15:17 by joscarlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,16 @@ void	move_player(t_cub3d *cub3d, double speed, int signal)
 		&& (cub3d->player.y + y_speed + get_signal(y_speed)) < (cub3d->map_y
 			- WALL_OFFSET))
 		cub3d->player.y += y_speed;
+}
+
+void	rotate_player(t_cub3d *cub3d, double angle)
+{
+	double	old_x;
+
+	old_x = cub3d->dir.x;
+	cub3d->dir.x = cub3d->dir.x * cos(angle) - cub3d->dir.y * sin(angle);
+	cub3d->dir.y = old_x * sin(angle) + cub3d->dir.y * cos(angle);
+	old_x = cub3d->plane.x;
+	cub3d->plane.x = cub3d->plane.x * cos(angle) - cub3d->plane.y * sin(angle);
+	cub3d->plane.y = old_x * sin(angle) + cub3d->plane.y * cos(angle);
 }
