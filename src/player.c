35ea6ec96ae6be1f9510c1fd18f78f5af75737b5
ZekaEu <zeka.surf@hotmail.com>
2025-02-25 21:09:50 -0300

    feat: implement move_player method at player.c file

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 18:52:21 by joscarlo          #+#    #+#             */
-/*   Updated: 2025/02/25 18:56:17 by joscarlo         ###   ########.fr       */
+/*   Updated: 2025/02/25 20:36:05 by joscarlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,29 @@ int	valid_player(char *line)
 		line++;
 	}
 	return (num);
+}
+
+void	move_player(t_cub3d *cub3d, double speed, int signal)
+{
+	double	x_speed;
+	double	y_speed;
+
+	if (signal == 0)
+	{
+		x_speed = cub3d->dir.x * speed;
+		y_speed = cub3d->dir.y * speed;
+	}
+	else
+	{
+		x_speed = -signal * cub3d->dir.y * speed;
+		y_speed = signal * cub3d->dir.x * speed;
+	}
+	if ((cub3d->player.x + x_speed + get_signal(x_speed)) > WALL_OFFSET
+		&& (cub3d->player.x + x_speed + get_signal(x_speed)) < (cub3d->map_x
+			- 1 - WALL_OFFSET))
+		cub3d->player.x += x_speed;
+	if ((cub3d->player.y + y_speed + get_signal(y_speed)) > WALL_OFFSET
+		&& (cub3d->player.y + y_speed + get_signal(y_speed)) < (cub3d->map_y
+			- WALL_OFFSET))
+		cub3d->player.y += y_speed;
 }
